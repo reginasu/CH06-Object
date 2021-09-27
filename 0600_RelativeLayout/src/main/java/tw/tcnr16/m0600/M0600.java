d34eb59104debdee41431700c7c2256af0969af5
您的名字 <rsu04122021@gmail.com>
2021-09-28 07:27:41 +0800

    0928

---
+++ 0600_RelativeLayout/src/main/java/tw/tcnr16/m0600/M0600.java
@@ -0,0 +1,14 @@
+package tw.tcnr16.m0600;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class M0600 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.m0600);
+    }
+}
